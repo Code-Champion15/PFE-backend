cf3210fefccef143e577f9eeb71224398120fcb9
yasmine alaya <alayayesmine12@gmail.com>
2025-03-21 11:30:25 +0100

    update pageRoute.js

--- backend/routes/pageRoute.js
+++ backend/routes/pageRoute.js
@@ -4,8 +4,11 @@ const pageController = require('../Controllers/pageController');
 
 router.post('/', pageController.createPage);
 router.get('/', pageController.getPages);
+router.get('/withchildren', pageController.getAllPagesWithChildren);
 router.get('/:id', pageController.getPageById);
+router.get('/byroute/:route', pageController.getPageByRoute);
+router.get('/content/:id', pageController.getContentById);
+router.get('/contentbyroute/:route',pageController.getContentByRoute)
 router.put('/:id', pageController.updatePage);
 router.delete('/:id', pageController.deletePage);
-
 module.exports = router;
